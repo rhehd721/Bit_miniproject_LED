76b9e5b958ab43857d366d1cfa774b690177c0e0
알파카 <rhehd721@naver.com>
2020-11-03 16:39:35 +0900

    miniproject

---
+++ FlaskEx/models.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db=SQLAlchemy()
+
+class Myuser(db.Model):
+    __tablename__='myuser'
+    id = db.Column(db.Integer,primary_key=True)
+    password = db.Column(db.String(64))
+    userid = db.Column(db.String(32))
+    username=db.Column(db.String(8))
+
+# LED 관련 DB
+class LED(db.Model):
+    __tablename__='LED'
+    id = db.Column(db.Integer,primary_key=True)
+    red = db.Column(db.String(2))
+    green = db.Column(db.String(2))
+    yellow =db.Column(db.String(2))
+
